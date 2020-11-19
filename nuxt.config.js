5a582e2e0f4aad9b4ccd73f765935278633932a6
Govin <360996299@qq.com>
2020-11-19 17:55:39 +0800

    更新文件 nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: '海关HSCode编码查询',
+    title: 'HSCode编码查询-首页',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
