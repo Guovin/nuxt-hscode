455d27ba4c9f42121fecba52854388dc45705e38
govin <360996299@qq.com>
2020-12-28 15:55:39 +0800

    URI

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,8 @@
         </el-breadcrumb>
         <!-- logo区域 -->
         <div class="logo_container">
-          <span class="logo" @click="goHome">
+          <span class="logo"
+                @click="goHome">
             <span class="hs">HS</span>Code
           </span>
         </div>
@@ -19,22 +20,33 @@
       <el-main>
         <el-card class="searchCard">
           <!-- 搜索区域 -->
-          <el-input placeholder="请输入商品名称或商品编码" v-model="key" @change="inputChange" clearable>
-            <el-button slot="append" icon="el-icon-search" @click="getKey"></el-button>
+          <el-input placeholder="请输入商品名称或商品编码"
+                    v-model="key"
+                    @change="inputChange"
+                    clearable>
+            <el-button slot="append"
+                       icon="el-icon-search"
+                       @click="getKey"></el-button>
           </el-input>
         </el-card>
         <nuxt-child ref="child" />
       </el-main>
       <el-footer class="footer">
-        copyright <a href="https://www.hscode.vip" target="blank">www.hscode.vip</a> 版权归 HSCode编码网 <a href="#"
-          @click="drawer = true">网站声明</a>
+        copyright <a href="https://www.hscode.vip"
+           target="blank">www.hscode.vip</a> 版权归 HSCode编码网 <a href="#"
+           @click="drawer = true">网站声明</a>
         <div class="note">本站所有数据仅供学习与参考，如有疑问，请联系360996299@qq.com！</div>
       </el-footer>
     </el-container>
     <!-- 回到顶部 -->
-    <el-backtop target=".home" :visibility-height="150"></el-backtop>
+    <el-backtop target=".home"
+                :visibility-height="150"></el-backtop>
     <!-- 网站声明 -->
-    <el-drawer title="免责声明" size="250px" :visible.sync="drawer" :direction="direction" :before-close="handleClose">
+    <el-drawer title="免责声明"
+               size="250px"
+               :visible.sync="drawer"
+               :direction="direction"
+               :before-close="handleClose">
       <div>
         1、HSCode编码网(www.hscode.vip)提醒您：在使用HSCode编码网提供的任何服务前，请务必仔细阅读并透彻理解本免责声明，您可以选择不使用HSCode编码网提供的服务，但如果您使用HSCode编码网的任何服务，您的使用行为将被视为对本声明全部内容的完全认可。
         <br>
@@ -49,89 +61,89 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        // 关键词
-        key: '',
-        drawer: false,
-        direction: 'btt',
-      }
+export default {
+  data () {
+    return {
+      // 关键词
+      key: '',
+      drawer: false,
+      direction: 'btt',
+    }
+  },
+  methods: {
+    // 根据关键词获取数据列表
+    getKey () {
+      this.$router.push({
+        path: 'table',
+        query: {
+          key: encodeURIComponent(this.key).replace(/%/g, '%25')
+        }
+      })
     },
-    methods: {
-      // 根据关键词获取数据列表
-      getKey() {
-        this.$router.push({
-          path: 'table',
-          query: {
-            key: encodeURIComponent(this.key)
-          }
-        })
-      },
-      //输入框change事件
-      inputChange() {
-        if (this.$route.path === '/table') {
-          return this.$refs['child'].getListByKey(encodeURIComponent(this.key))
-        }
-        this.getKey()
-      },
-      //网站声明抽屉
-      handleClose(done) {
-        done()
-      },
-      //返回首页
-      goHome() {
-        if (this.$route.path !== '/') {
-          return this.$router.push('/')
-        }
+    //输入框change事件
+    inputChange () {
+      if (this.$route.path === '/table') {
+        return this.$refs['child'].getListByKey(encodeURIComponent(this.key).replace(/%/g, '%25'))
+      }
+      this.getKey()
+    },
+    //网站声明抽屉
+    handleClose (done) {
+      done()
+    },
+    //返回首页
+    goHome () {
+      if (this.$route.path !== '/') {
+        return this.$router.push('/')
       }
     }
   }
+}
 </script>
 
 <style scoped>
-  .home {
-    height: 100%;
-    overflow: auto;
-    display: flex;
-    min-height: 100vh;
-  }
+.home {
+  height: 100%;
+  overflow: auto;
+  display: flex;
+  min-height: 100vh;
+}
 
-  .el-header {
-    height: 100px !important;
-  }
+.el-header {
+  height: 100px !important;
+}
 
-  .el-card {
-    margin: auto;
-    margin-top: 50px;
-    margin-bottom: 30px;
-    width: 50%;
-  }
+.el-card {
+  margin: auto;
+  margin-top: 50px;
+  margin-bottom: 30px;
+  width: 50%;
+}
 
-  .el-breadcrumb {
-    margin-top: 10px;
-  }
+.el-breadcrumb {
+  margin-top: 10px;
+}
 
-  .footer {
-    width: 100%;
-    text-align: center;
-    font-size: 14px;
-    background-color: #1F2939;
-    height: 30px;
-    line-height: 30px;
-  }
+.footer {
+  width: 100%;
+  text-align: center;
+  font-size: 14px;
+  background-color: #1f2939;
+  height: 30px;
+  line-height: 30px;
+}
 
-  .note {
-    font-size: 10px;
-  }
+.note {
+  font-size: 10px;
+}
 
-  .el-drawer div {
-    font-size: 10px;
-    margin-left: 10px;
-  }
+.el-drawer div {
+  font-size: 10px;
+  margin-left: 10px;
+}
 
-  .searchCard {
-    background-color: rgba(255, 255, 255, 0.01);
-    border: rgba(255, 255, 255, 0.01)
-  }
+.searchCard {
+  background-color: rgba(255, 255, 255, 0.01);
+  border: rgba(255, 255, 255, 0.01);
+}
 </style>
