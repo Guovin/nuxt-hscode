4c6d46603a125335f186a3665637ab8e208d9128
Such <360996299@qq.com>
2021-01-01 23:05:21 +0800

    use element-ui CDN

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,10 +10,12 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/hscode.png' },
+      { rel: 'stylesheet', href: 'https://unpkg.com/element-ui/lib/theme-chalk/index.css' },
       { rel: 'stylesheet', href: 'https://cdn.staticfile.org/nprogress/0.2.0/nprogress.min.css' }
     ],
     script: [
       { src: 'https://cdn.staticfile.org/vue/2.5.22/vue.min.js' },
+      { src: 'https://unpkg.com/element-ui/lib/index.js' },
       { src: 'https://cdn.staticfile.org/axios/0.18.0/axios.min.js' },
       { src: 'https://cdn.staticfile.org/nprogress/0.2.0/nprogress.min.js' }
     ]
@@ -49,7 +51,8 @@ export default {
         config.externals = {
           vue: 'Vue',
           axios: 'axios',
-          nprogress: 'NProgress'
+          nprogress: 'NProgress',
+          'element-ui': 'element-ui'
         }
       }
     },
